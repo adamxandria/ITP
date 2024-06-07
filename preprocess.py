d803525de696aa06c92caad784ed2010550778b3
steady <51741102+iissteadyone@users.noreply.github.com>
2024-06-07 14:19:48 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,81 @@
 import cv2
-import face_recognition
 import os
 import matplotlib.pyplot as plt
+import math
+from sklearn import neighbors
+import os.path
+import pickle
+from PIL import Image, ImageDraw, ImageFont
+import face_recognition
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
+
+TEST_DIR = "test"
+OUTPUT_DIR = "output"
+KNOWN_ENCODED_IMAGES = {}
+
+
+# To incorporate the face comparison function
+def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.35):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+
+    :param X_img_path: path to image to be recognized
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    """
+    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
+        raise Exception("Invalid image path: {}".format(X_img_path))
+
+    if knn_clf is None and model_path is None:
+        raise Exception("Must supply knn classifier either through knn_clf or model_path")
+
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # Load image file and find face locations
+    X_img = face_recognition.load_image_file(X_img_path)
+    X_face_locations = face_recognition.face_locations(X_img)
+
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the test image
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+def crop_and_save(predictions, img_path):
+    # Load image
+    img = face_recognition.load_image_file(img_path)
+
+    # Get file name
+    img_name = img_path.split('\\')[-1]
+
+    # Saving cropped image to their respective output folders, ignores faces predicted as unknown
+    for name, (top, right, bottom, left) in predictions:
+        if name != "unknown":
+            output_dest = f'{OUTPUT_DIR}/{name}'
+            if not os.path.exists(output_dest):
+                os.mkdir(output_dest)
+            faceImg = img[top:bottom, left:right]
+            final = Image.fromarray(faceImg)
+            final.save(f"{output_dest}/{img_name}")
+    return
 
 
 def encode_face(image_path):
@@ -15,6 +89,10 @@ def encode_face(image_path):
         raise ValueError(f"No faces found in {image_path}")
 
 
+# Change such that if prediction = true, get label along with target encoding,
+# then crop, save and load encoded image into this function,
+# if not same, discard the image and return as unknown, else just return label and not delete the cropped image,
+# have to verify the accuracy of the face comparison though,
 def detect_and_crop_target_face(image_path, target_encoding, output_path):
     # Load the image
     img = cv2.imread(image_path)
@@ -63,8 +141,11 @@ for input_path, output_path in zip(input_paths, output_paths):
         cropped_images.append(cropped_image)
 
 # Display the cropped images
-for cropped_image in cropped_images:
-    rgb_cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
-    plt.imshow(rgb_cropped_image)
-    plt.axis('off')
-    plt.show()
+# for cropped_image in cropped_images:
+#     rgb_cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+#     plt.imshow(rgb_cropped_image)
+#     plt.axis('off')
+#     plt.show()
+
+# Main function to get encoded images and store into a global variable
+# Possibly store in a dictionary
